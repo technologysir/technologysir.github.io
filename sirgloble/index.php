fa62585d45089f46aecd02a33f88b3ceab063201
sir <112639200+technologysir@users.noreply.github.com>
2023-01-12 09:58:02 +0100

    try

---
+++ sirgloble/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>|sirtecnology</title>
+</head>
+<body>
+
+<button id="bt">submit</button>
+<h1 id="h1">hello</h1>
+<script src="./js/script.js"></script>
+</body>
+</html>
